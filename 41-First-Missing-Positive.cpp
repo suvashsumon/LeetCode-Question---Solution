a39a52a04e3f80e614bc5c5e0e7f52c492c1b153
Suvash Kumar Sumon <suvashkumar.naogaon@gmail.com>
2024-03-26 11:22:55 +0600

    Create 41-First-Missing-Positive.cpp

---
+++ 41-First-Missing-Positive.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int len = nums.size();
+        for(int i=0; i<len; i++)
+        {
+            if(nums[i]>0 && nums[i]<=len && nums[i]!=nums[nums[i]-1]) 
+            {
+                swap(nums[i], nums[nums[i]-1]);
+                i--;
+            }
+        }
+        for(int i=0; i<len; i++) cout << nums[i] << endl;
+        for(int i=0; i<len; i++)
+        {
+            if(nums[i]!=i+1) return i+1;
+        }
+        return len+1;
+    }
+};
