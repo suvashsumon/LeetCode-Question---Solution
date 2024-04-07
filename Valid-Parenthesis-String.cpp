d9a7c16d45c4d02a2391b763d62d73eed18401bb
Suvash Kumar Sumon <suvashkumar.naogaon@gmail.com>
2024-04-07 21:04:07 +0600

    Create Valid-Parenthesis-String.cpp

---
+++ Valid-Parenthesis-String.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int len = s.size();
+        stack<int>left, star;
+        for(int i=0; i<len; i++)
+        {
+            if(s[i]=='(') left.push(i);
+            else if(s[i]=='*') star.push(i);
+            else
+            {
+                if(!left.empty()) left.pop();
+                else if(!star.empty()) star.pop();
+                else return false;
+            }
+        }
+
+        while(!left.empty())
+        {
+            if(star.empty()) return false;
+            if(left.top()<star.top())
+            {
+                left.pop();
+                star.pop();
+            }
+            else return false;
+        }
+        return true;
+    }
+};
